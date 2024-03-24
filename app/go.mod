9e234c358d30b28c0e447585c1e8adae6401cf40
alicey <sirius@alicey.dev>
2024-03-24 21:51:22 +0900

    show vm list

--- app/go.mod
+++ app/go.mod
@@ -1,3 +1,5 @@
 module alicey.dev/proxctl
 
 go 1.22.1
+
+require github.com/Telmate/proxmox-api-go v0.0.0-20240318115458-a76eb2351c37 // indirect
--- app/main.go
+++ app/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Telmate/proxmox-api-go/proxmox"
+)
 
 func main() {
 	fmt.Println("wow proxctl!")
+
+	prox_api_url := os.Getenv("PROXMOX_API_URL")
+	prox_user := os.Getenv("PROXMOX_USERNAME")
+	prox_pw := os.Getenv("PROXMOX_PASSWORD")
+
+	if prox_api_url == "" || prox_user == "" || prox_pw == "" {
+		log.Fatal("PROXMOX_API_URL, PROXMOX_USERNAME, PROXMOX_PASSWORD environment variables are not set")
+	}
+
+	// 証明書検証無効にする
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	httpClient := &http.Client{Transport: tr}
+
+	client, err := proxmox.NewClient(prox_api_url, httpClient, "", nil, "", 60)
+	if err != nil {
+		log.Fatal("Failed to create Proxmox client: ", err)
+	}
+
+	err = client.Login(prox_user, prox_pw, "")
+	if err != nil {
+		log.Fatal("Failed to log in to Proxmox: ", err)
+	}
+
+	// show vm list
+	vmList, err := client.GetVmList()
+	if err != nil {
+		log.Fatal("Failed to get VM list: ", err)
+	}
+
+	fmt.Println("VM List:")
+	for key, value := range vmList {
+		fmt.Printf("Key: %s, Value: %+v\n", key, value)
+	}
+
 }
